23fb83c9a4febbd2a63869f4b187abbeed418086
mantrabrain <mantrabrain@gmail.com>
2020-02-02 22:37:00 +0545

    update

---
+++ mantrabrain-theme1/breadcrumbs/class-mantrabrain-theme-breadcrumbs.php
@@ -0,0 +1,31 @@
+<?php
+if (!class_exists('Mantrabrain_Theme_Breadcrumbs')) {
+
+    class Mantrabrain_Theme_Breadcrumbs
+    {
+        private static $instance = false;
+
+        public static function get_instance()
+        {
+
+            if (!isset(self::$instance)) {
+                self::$instance = new self;
+            }
+            return self::$instance;
+        }
+
+        public function breadcrumb_trail($args = array())
+        {
+            include_once "class-breadcrumb-trail.php";
+
+            $breadcrumb = apply_filters('mantrabrain_theme_breadcrumb_trail_object', null, $args);
+
+            if (!is_object($breadcrumb))
+
+                $breadcrumb = new Breadcrumb_Trail($args);
+
+            return $breadcrumb->trail();
+        }
+
+    }
+}
